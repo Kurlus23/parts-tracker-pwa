b0a4eee2f76bf61dcb5640463f1ac4abf20fbba8
Kurlus23 <KEVIN.ELLIS@siemens-healthineers.com>
2026-03-19 23:43:30 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// IMPORTANT: base must match your GitHub Pages repo name
 export default defineConfig({
   base: '/parts-tracker-pwa/',
   plugins: [
